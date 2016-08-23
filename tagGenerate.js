77273e1be554c645212f14fc9baac7a19180bc7c
zgy03916618421 <576428698@qq.com>
2016-08-23 15:42:35 +0800

    buffer

--- tagGenerate.js
+++ tagGenerate.js
@@ -239,7 +239,7 @@ exports.imgMake = function (data,username,buf) {
             })
             var url = data[2];
             console.log(url);
-            hlTitleImg.src = new buffer (buf,'binary');
+            hlTitleImg.src = new Buffer (buf,'binary');
             hlTitleImg.onload = function () {
                 context.drawImage(hlTitleImg,100,318);
                 var stream = canvas.createPNGStream();
